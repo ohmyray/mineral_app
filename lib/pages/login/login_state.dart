8dd7f000a3ab95776450e8d3f01dc1ae9db2f507
Ray <ohmyray@163.com>
2021-11-08 19:06:28 +0800

    移除不需要的页面

--- lib/pages/login/login_state.dart
+++
@@ -1,8 +0,0 @@
-/// @description:
-/// @author 
-/// @date: 2021/11/07 08:40:27
-class LoginState {
-  LoginState() {
-    ///Initialize variables
-  }
-}
--- lib/pages/register/register_state.dart
+++
@@ -1,8 +0,0 @@
-/// @description:
-/// @author 
-/// @date: 2021/11/07 09:12:47
-class RegisterState {
-  RegisterState() {
-    ///Initialize variables
-  }
-}
