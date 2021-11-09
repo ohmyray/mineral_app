a1b26e2629742d6b8bf17dd9450158efbe8cc0ab
Ray <ohmyray@163.com>
2021-11-09 15:04:31 +0800

    添加地图点击marker

--- lib/pages/map/state.dart
+++ lib/pages/map/state.dart
@@ -1,8 +1,13 @@
 import 'package:get/get.dart';
+import 'package:latlong2/latlong.dart';
 
 class AMapState {
   // 当前 tab 页码
   final _page = 0.obs;
+  final _point = LatLng(51.5, -0.09).obs;
+
+  get point => this._point.value;
+  set point(value) => this._point.value = value;
   set page(value) => this._page.value = value;
   get page => this._page.value;
 }
--- lib/pages/map/view.dart
+++ lib/pages/map/view.dart
@@ -9,32 +9,44 @@ class AMapPage extends GetView<AMapController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FlutterMap(
-        options: MapOptions(
-          center: LatLng(51.5, -0.09),
-          zoom: 13.0,
-        ),
-        layers: [
-          TileLayerOptions(
-            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-            subdomains: ['a', 'b', 'c'],
-            attributionBuilder: (_) {
-              return Text("© OpenStreetMap contributors");
-            },
-          ),
-          MarkerLayerOptions(
-            markers: [
-              Marker(
-                width: 80.0,
-                height: 80.0,
-                point: LatLng(51.5, -0.09),
-                builder: (ctx) => Container(
-                  child: FlutterLogo(),
+      body: Obx(
+        () => Stack(
+          children: [
+            FlutterMap(
+              options: MapOptions(
+                  center: LatLng(51.5, -0.09),
+                  zoom: 13.0,
+                  onTap: (tapPosition, point) {
+                    print('$tapPosition,$point');
+                    print('$controller.state.point,$point');
+                    controller.state.point = point;
+                  }),
+              layers: [
+                TileLayerOptions(
+                  urlTemplate:
+                      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                  subdomains: ['a', 'b', 'c'],
+                  attributionBuilder: (_) {
+                    return Text("© OpenStreetMap contributors");
+                  },
                 ),
-              ),
-            ],
-          ),
-        ],
+                MarkerLayerOptions(
+                  markers: [
+                    Marker(
+                      width: 80.0,
+                      height: 80.0,
+                      point: controller.state.point,
+                      builder: (ctx) => const Icon(
+                        Icons.location_on,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
